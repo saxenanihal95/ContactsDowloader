1dbc3a727fc2a3d77d4738e309d55b340b40594b
saxeananihal95 <saxeananihal95@gmail.com>
2018-03-06 07:24:40 +0530

    added contact file

---
+++ app/src/main/java/com/notnull/nsaxena/contactsbrowser/Contact.java
@@ -0,0 +1,36 @@
+package com.notnull.nsaxena.contactsbrowser;
+
+/**
+ * Created by nsaxena on 6/3/18.
+ */
+
+public class Contact {
+
+    int id;
+    String name;
+    String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
--- app/src/main/java/com/notnull/nsaxena/contactsbrowser/MainActivity.java
+++ app/src/main/java/com/notnull/nsaxena/contactsbrowser/MainActivity.java
@@ -2,12 +2,26 @@ package com.notnull.nsaxena.contactsbrowser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import io.reactivex.subjects.PublishSubject;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button mGetContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGetContact=findViewById(R.id.getContact);
+
+        mGetContact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
+        });
     }
 }
